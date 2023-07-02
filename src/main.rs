e85d432e7b9caccaef44200d87ac7fcf84c23a5c
Alan <alan@alanklas.com>
2023-07-02 17:08:06 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
+
+#[derive(Component)]
+struct Player;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill the entire browser window
+                fit_canvas_to_parent: true,
+                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::rgb(0.15, 0.15, 0.15)))
+        .add_startup_systems((setup, spawn_player))
+        .add_systems((move_player,))
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        },
+    ));
+}
+
+fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Transform, With<Player>>) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
+        direction.y += 1.;
+    }
+    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
+
+    let move_speed = 0.13;
+    let move_delta = (direction * move_speed).extend(0.);
+
+    for mut transform in player_query.iter_mut() {
+        transform.translation += move_delta;
+    }
+}
