1bf889aa8493a5d8658a451a83cb8f007fc4b9df
Alan <alan@alanklas.com>
2023-07-05 00:12:09 +0200

    maybe voxelnetwork domain will help (no wss?)

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,13 @@ const LOCAL: bool = true;
 const ROOM_IP: &str = if LOCAL {
     "localhost"
 } else {
-    "matchbox.ducky.pics"
+    "matchbox.voxelnetwork.pl"
 };
 
-const WS_OR_WSS: &str = if LOCAL { "ws" } else { "wss" };
+// const WS_OR_WSS: &str = if LOCAL { "ws" } else { "wss" };
+// For voxelnetwork.pl it's always ws
+const WS_OR_WSS: &str = "ws";
+
 const ROOM_PORT: u16 = 7778;
 
 #[derive(Component)]
